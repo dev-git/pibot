a747b626d509eda432b512561b628b70923bfc58
dev-git <dev-git@users.noreply.github.com>
2015-08-14 14:33:39 +1200

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,21 @@ GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+
+GPIO.output(7, True)
+time.sleep(1)
+GPIO.output(7, False)
+
+GPIO.output(11, True)
+time.sleep(1)
+GPIO.output(11, False)
+
+GPIO.output(13, True)
+time.sleep(1)
+GPIO.output(13, False)
+
+GPIO.output(15, True)
+time.sleep(1)
+GPIO.output(15, False)
+
+GPIO.cleanup()
