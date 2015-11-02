35f3285e9a3b01208a50b3a0a40204b39c494ffd
dev-git <dev-git@users.noreply.github.com>
2015-11-02 20:57:21 +1300

    Create module4.py

---
+++ module4.py
@@ -0,0 +1,33 @@
+# Traffic light prog
+import RPi.GPIO as GPIO
+import time
+
+print 'PiBot initialisation code routine...'
+
+# What Raspberry Pi revision are we running?  
+GPIO.RPI_REVISION     #  0 = Compute Module, 1 = Rev 1, 2 = Rev 2, 3 = Model B+  
+  
+# What version of RPi.GPIO are we running?  
+GPIO.VERSION   
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+GPIO.output(11, True)
+time.sleep(2)
+GPIO.output(11, False)
+
+GPIO.output(13, True)
+time.sleep(2)
+GPIO.output(13, False)
+
+GPIO.output(15, True)
+time.sleep(2)
+GPIO.output(15, False)
+
+GPIO.cleanup()
+
+print 'Program complete.'
