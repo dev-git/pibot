a7c23e73a7149206f3c6783ca5c25420fff8a82d
dev-git <dev-git@users.noreply.github.com>
2015-11-03 14:26:13 +1300

    Update module4-1.py

--- module4-1.py
+++ module4-1.py
@@ -2,9 +2,8 @@
 
 import RPi.GPIO as GPIO
 import time
-import sys, tty, termios
 
-print 'Hit X to exit the program'
+print 'Press CTRL+C to exit the program'
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -12,32 +11,11 @@ GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
-# The getch method can determine which key has been pressed
-# by the user on the keyboard by accessing the system files
-# It will then return the pressed key as a variable
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
 RUNNING = True
 
 try:
    while RUNNING:
-    # Keyboard character retrieval method is called and saved
-    # into variable
-    #char = getch()
 
-    #if(char == "x"):
-    #    RUNNING = False
-        #print "\nPiBot is going offline."
-    #    break
-    
     print 'Turn on red light\n'
     GPIO.output(11, True)
     time.sleep(2)
@@ -51,12 +29,7 @@ try:
     print 'Turn on green light\n'    
     GPIO.output(15, True)
     time.sleep(2)
-    GPIO.output(15, False)
 
-    # The keyboard character variable will be set to blank, ready
-    # to save the next key that is pressed
-    #char = ""      
-      
 # If CTRL+C is pressed the main loop is broken
 except KeyboardInterrupt:
    RUNNING = False
